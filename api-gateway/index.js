9b2d78919f84faf09081169ffd9bb583f851cc23
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-10-23 22:18:49 +0530

    Added cors policy to api gateway service

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -5,9 +5,19 @@ const CircuitBreaker = require('opossum');
 const rateLimit = require('express-rate-limit');
 const jwt = require('jsonwebtoken');
 const winston = require('winston');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }
+));
+app.use(express.urlencoded({ extended: true }));
 
 // Logger setup
 const logger = winston.createLogger({
