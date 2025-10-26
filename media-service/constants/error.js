9f75bf9d6b42872bef8b1a7f08994cd15b77097b
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-10-26 17:19:51 +0530

    Updated type to module in api-gateway and fixed health check of notification service

--- media-service/constants/error.js
+++ media-service/constants/error.js
@@ -5,11 +5,11 @@ export const SUCCESS = {
 }
 
 export const ERROR = {
-    NOT_FOUND: "Not found",
-    ERROR_TOO_MANY_UPLOADS: "Too many uploads",
-    INTERNAL_SERVER_ERROR: "Internal server error",
-    ERROR_NO_FILE_UPLOADED: "No file uploaded",
-    ERROR_INVALID_FILE_TYPE: "Invalid file type",
-    ERROR_FILE_TOO_LARGE: "File too large",
-    ERROR_INVALID_FILE: "Invalid file",
+    NOT_FOUND: "Not found (Media Service)",
+    ERROR_TOO_MANY_UPLOADS: "Too many uploads (Media Service)",
+    INTERNAL_SERVER_ERROR: "Internal server error (Media Service)",
+    ERROR_NO_FILE_UPLOADED: "No file uploaded (Media Service)",
+    ERROR_INVALID_FILE_TYPE: "Invalid file type (Media Service)",
+    ERROR_FILE_TOO_LARGE: "File too large (Media Service)",
+    ERROR_INVALID_FILE: "Invalid file (Media Service)",
 }
